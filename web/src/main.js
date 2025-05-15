9b4029db151b80b503b310dc25dbec7d04e364eb
WesleyLamb <wesleylmb@gmail.com>
2025-05-15 11:21:27 -0300

    fix: adicionado icones no front

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';  // Importando Vuetify do arquivo plugins/vuetify.js
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false;
 
