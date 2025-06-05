d6a936fe3694ecc7935683820b812b585cee0925
fabricio bizotto <fabricio.bizotto@ifc.edu.br>
2025-06-05 10:39:42 -0300

    alteração no AuthController#login

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
     {
         $client = Client::where('password_client', 1)->first();
 
-        $http = Http::asForm()->post((env('APP_ENV') == 'production' ? 'https' : 'http') .'://auth.eventos/oauth/token', [
+        $http = Http::asForm()->post((env('APP_ENV') == 'production' ? 'http' : 'http') .'://reverse-proxy/oauth/token', [
             'grant_type' => 'password',
             'client_id' => $client->id,
             'client_secret' => $client->secret,
