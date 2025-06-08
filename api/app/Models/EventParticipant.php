2450244a1de39d97be66339a65208c20143b63c3
thalesfb <15121152+thalesfb@users.noreply.github.com>
2025-06-08 15:11:22 -0300

    :sparkles: feat: criar modelo EventParticipant para gerenciar participantes de eventos

---
+++ api/app/Models/EventParticipant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class EventParticipant extends Model
+{
+    protected $table = 'event_participants';
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'arrival_date',
+    ];
+
+    protected $dates = [
+        'arrival_date',
+    ];
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+} 
