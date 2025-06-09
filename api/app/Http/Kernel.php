d7f637eb5f6d1af40afdd3eb2fc02d175d14e328
thalesfb <15121152+thalesfb@users.noreply.github.com>
2025-06-09 16:13:19 -0300

    :bug: fix: corrigir definição de middleware 'organizer' para uso correto da classe EnsureUserHasPermission

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'permission' => EnsureUserHasPermission::class,
-        'organizer' => [EnsureUserHasPermission::class . ':organizer'],
+        'organizer' => EnsureUserHasPermission::class . ':organizer',
     ];
 }
