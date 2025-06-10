22c6e4965fbbd304c46be018ea4d3518ef440c92
WesleyLamb <wesleylmb@gmail.com>
2025-06-10 18:38:44 -0300

    fix: ajustes no seeder de eventos

--- api/app/Http/Resources/UserResource.php
+++ api/app/Http/Resources/UserResource.php
@@ -29,6 +29,7 @@ class UserResource extends JsonResource
             'phone' => $phone,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'permissions' => $this->permissions->pluck('permission')->toArray()
         ];
     }
 }
